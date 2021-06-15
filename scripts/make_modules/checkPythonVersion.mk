b9535b029b3849a72e2b2987811eaa8ba54c4fda
scirelli <scirelli+git@gmail.com>
2021-06-15 14:07:39 -0400

    Adding more features

--- scripts/make_modules/checkPythonVersion.mk
+++
@@ -1,7 +0,0 @@
-SHELL:=/usr/bin/env bash
-
-PYTHON3_VERSION=$(shell python3 --version | grep -oE '3\.[7-9]')
-
-ifeq "$(PYTHON3_VERSION)" ""
-$(error 'At least Python 3.7 is required.')
-endif
--- scripts/pre-push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o pipefail -o noclobber -o nounset
-
-make lint
-make test
